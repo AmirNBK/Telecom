54a0f7d3d22d717ae4eed8c5b9bcf304c2962e89
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-08-08 13:49:07 +0330

    linked news items to single news page

--- src/components/News/NewsItems/NewsItems.svelte
+++ src/components/News/NewsItems/NewsItems.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-    export let title : string;
-    export let date : string;
-    export let description : string;
+    export let title: string;
+    export let date: string;
+    export let description: string;
 </script>
 
-<div class="NewsItemContainer flex flex-col gap-10 pb-4" style="border-bottom: 1px solid gray;">
+<div
+    class="NewsItemContainer flex flex-col gap-10 pb-4"
+    style="border-bottom: 1px solid gray;"
+>
     <div
         class="NewsItemContainer__headline flex justify-between"
         style="color : #DC0600"
@@ -15,9 +18,12 @@
     <div class="font-medium text-sm sm:text-base">
         {description}
     </div>
-    <div class="text-end">
-        <button class="rounded-md py-2 px-6 w-fit text-sm mt-2 text-white" style="background-color: #9C1511;"
-            >details</button
-        >
-    </div>
+    <a href="/news/items">
+        <div class="text-end">
+            <button
+                class="rounded-md py-2 px-6 w-fit text-sm mt-2 text-white"
+                style="background-color: #9C1511;">details</button
+            >
+        </div>
+    </a>
 </div>
